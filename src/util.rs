2783c9d3c5bee8836b13ad711c5f12e5b1ccb9dd
Kyle Wood <demonwav@gmail.com>
2020-04-11 01:27:39 -0700

    Put f64.min(f64) back instead of min(f64, f64)
    I forgot that f64 doesn't implement Ord due to floating point NaN
    issues.

--- src/util.rs
+++ src/util.rs
@@ -19,7 +19,6 @@ use nix::errno::Errno::ESRCH;
 use nix::sys::signal::kill;
 use nix::unistd::Pid;
 use nix::Error;
-use std::cmp::min;
 use std::num::ParseIntError;
 use std::path::PathBuf;
 use std::{env, fs, io};
@@ -88,7 +87,7 @@ pub fn find_prog(searches: &[(&str, &str)]) -> Option<PathBuf> {
 }
 
 pub fn tps_cap(tps: f64) -> f64 {
-    return min(tps, 20.0);
+    return tps.min(20.0);
 }
 
 pub trait ExitError<T> {
